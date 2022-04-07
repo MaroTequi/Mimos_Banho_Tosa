74b8ab8077221473f2d40eaf012cb423194ed60f
Maurício <mauriciof2408@gmail.com>
2022-04-07 19:36:02 -0300

    Adicionando aula 'ConversãoDeTipos'

---
+++ ConversãoDeTipos.js
@@ -0,0 +1,15 @@
+// = Atribuição
+const number = 1;
+const word = "1"
+
+// == igualdade de valor
+console.log(number == word);
+
+// === igualdade de valor e tipo
+console.log(number === word);
+
+//conversão implícita
+console.log(number + word);
+
+//conversão explícita
+console.log(number + +word);
